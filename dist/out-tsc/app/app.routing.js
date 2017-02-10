0b4f638b727ba9019af0272d45d108e7fa57158c
Mike Reardon <michaelpjreardon@gmail.com>
2017-02-10 19:36:52 +0000

    Implement redirects for user route with no ID and undefined routes

--- dist/out-tsc/app/app.routing.js
+++ dist/out-tsc/app/app.routing.js
@@ -3,9 +3,11 @@ import { HomeComponent } from "./home-component.component";
 import { UserComponent } from "./user/user.component";
 import { USER_ROUTES } from "./user/user.routes";
 var APP_ROUTES = [
+    { path: 'user', redirectTo: '/user/1', pathMatch: 'full' },
     { path: 'user/:id', component: UserComponent },
     { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '/user/1', pathMatch: 'full' }
 ];
 export var routing = RouterModule.forRoot(APP_ROUTES);
 //# sourceMappingURL=../../../src/app/app.routing.js.map
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,11 @@ import { UserComponent } from "./user/user.component";
 import { USER_ROUTES} from "./user/user.routes";
 
 const APP_ROUTES: Routes = [
+  { path: 'user', redirectTo: '/user/1', pathMatch: 'full'},
   { path: 'user/:id', component: UserComponent },
   { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-  { path: '',     component: HomeComponent }
+  { path: '',     component: HomeComponent },
+  { path: '**', redirectTo: '/user/1', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
